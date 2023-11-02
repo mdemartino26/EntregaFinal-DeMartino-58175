13281ccf1e0b5f46fa0d8c39af7c354a3692ca76
mdemartino26 <m.demartino99@gmail.com>
2023-11-01 22:38:17 -0300

    acomodando archivos y components

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import CartWidget from "../CartWidget/CartWidget";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from 'react-bootstrap/Button';
+import './NavBar.css';
+
+
+const NavBar = () => {
+  return (
+    <Navbar className="navbar">
+      <Container>
+        <Nav>
+          <h3>CoShop</h3>
+        </Nav>
+        <Nav>
+          <div>
+            <Button  variant="outline-primary">Celulares</Button>
+            <Button  variant="outline-primary">Tablet</Button>
+            <Button  variant="outline-primary">Notebooks</Button>
+          </div>
+        </Nav>
+        <Nav>
+          <CartWidget />
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
