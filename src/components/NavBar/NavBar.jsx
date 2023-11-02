20a268dbc47bf081dbf7db29c2db35ebd3863f44
mdemartino26 <m.demartino99@gmail.com>
2023-11-01 22:43:33 -0300

    install bootsrap y estilos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from 'react-bootstrap/Button';
 import './NavBar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const NavBar = () => {
