53725e6caf6b1385c95de2200c48773748a5885e
mdemartino26 <m.demartino99@gmail.com>
2023-11-20 23:20:45 -0300

    contador con boton, error de estilos en app y greeting

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
 
   return (
     <>
-        <div className="App">
+    <div className="App">
       <NavBar/>
       <ItemListContainer greeting={'Bienvenidos'}/>
+      <ItemCount initial={0} stock={10} onAdd={(quantity) => console.log('Catidad agregada ', quantity)}/>
     </div>
     </>
   )
