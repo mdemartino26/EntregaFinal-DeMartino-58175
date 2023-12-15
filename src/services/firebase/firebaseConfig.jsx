73315c4c523c76cd608bc8517bb8b2b29f8edcbd
mdemartino26 <m.demartino99@gmail.com>
2023-12-15 20:07:03 -0300

    .env .gitIgnore y firebaseconfig

--- src/services/firebase/firebaseConfig.jsx
+++ src/services/firebase/firebaseConfig.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIMbgF3s4-Ffa22OkqCAmeclfrzeIo_n8",
-  authDomain: "coderhouse-ecommerce-demartino.firebaseapp.com",
-  projectId: "coderhouse-ecommerce-demartino",
-  storageBucket: "coderhouse-ecommerce-demartino.appspot.com",
-  messagingSenderId: "82130524658",
-  appId: "1:82130524658:web:4a62308f02a0a4bb05b36b"
-};
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
